31c6ad92741fff0a7fbf9d25108b20702cd71545
xiongxeng <xiongxeng@gmail.com>
2024-01-28 12:24:59 -0600

    add databse to do, add packages

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,69 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("put to database");
+
+  // Open indexedDB database
+  const db = await initDB();
+
+  // Start a transaction in readwrite mode
+  const tx = db.transaction("jate", "readwrite");
+
+  // Access the object store "jate"
+  const store = tx.objectStore("jate");
+
+ // Add the content to the object store 
+  const request = store.add({ jate: content });
+
+  try {
+    // Wait for the transaction to complete
+    const results = await request;
+    // Log the results
+    console.log("Data saved to the database", results);
+  } catch (error) {
+    // Catch any errors
+    console.error("Error saving data to the database", error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("get from database");
+
+   // Open indexedDB database
+  const db = await initDB();
+
+   // Start a transaction in readonly mode
+  const tx = db.transaction("jate", "readonly");
+
+  // Access the object store "jate"
+  const store = tx.objectStore("jate");
+
+  // Get by id or get all content
+  const request = id ? store.get(id) : store.getAll();
+
+  try {
+    // Wait for the transaction to complete
+    const results = await request;
+    // Log the results
+    console.log("Data retrieved from the database", results);
+  } catch (error) {
+    // Catch any errors
+    console.error("Error retrieving data from the database", error);
+  }
+};
 
 initdb();
